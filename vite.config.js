4a44d249168059c60bf07719bea20c34ea6557cc
Sabri_Efe <se.zybk@hotmail.com>
2026-05-15 09:35:00 +0300

    vercel base fix

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react";
 
-// <https://vitejs.dev/config/>
 export default defineConfig({
-  base: "/goit-react-hw-01/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-    rolldownOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
-    },
-  },
+  base: "/",
 });
