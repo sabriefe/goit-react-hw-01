9743cf071b3c2378d903544279d53a1870502fa6
Sabri_Efe <se.zybk@hotmail.com>
2026-05-16 05:44:27 +0300

    files names updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import Profiles from "./components/profiles/Profiles.jsx";
-import FriendList from "./components/friendlist/FriendList.jsx";
-import TransactionHistory from "./components/transactionhistory/TransactionHistory.jsx";
+import Profiles from "./components/Profiles/Profiles.jsx";
+import FriendList from "./components/FriendList/FriendList.jsx";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx";
 import userData from "./userData.json";
 import friends from "./friends.json";
 import transactions from "./transaction.json";
